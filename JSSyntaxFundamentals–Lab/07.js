730046f584e03d5136b55b980891a08541a948dd
Ivan Peshterski <peshterskiivan@gmail.com>
2026-02-21 14:07:16 +0200

    Second part done

---
+++ JSSyntaxFundamentals–Lab/07.js
@@ -0,0 +1,51 @@
+function theatre(day, age) {
+
+    let price = 0;
+
+    if(age<0) {
+        console.log('Error!');
+        return;
+    }
+
+    switch(day) {
+        case 'Weekday': 
+            if(age>=0 && age <=18) {
+                price = 12;
+            }
+            else if(age > 18 && age <= 64) {
+                price = 18;
+            }
+            else if(age > 64 && age <=122) {
+                price = 12;
+            }
+        break;
+
+        case 'Weekend': 
+            if(age>=0 && age <=18) {
+                price = 15;
+            }
+            else if(age > 18 && age <= 64) {
+                price = 20;
+            }
+            else if(age > 64 && age <=122) {
+                price = 15;
+            }
+
+        break;
+
+        case 'Holiday': 
+            if(age>=0 && age <=18) {
+                price = 5;
+            }
+            else if(age > 18 && age <= 64) {
+                price = 12;
+            }
+            else if(age > 64 && age <=122) {
+                price = 10;
+            }
+        break;
+    }
+
+    console.log(`${price}$`);
+}
+theatre('Weekday', -42);
